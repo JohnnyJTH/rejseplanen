abcb5eeda3f5350589cd7188eb23cf62d96adc4f
Johnny JTH <johanohly@gmail.com>
2023-01-05 20:17:15 +0100

    Fixed the redirect.

--- src/routes/timetable/+page.svelte
+++ src/routes/timetable/+page.svelte
@@ -1,4 +1,6 @@
-<script>
-	import { goto } from '$app/navigation';
-	goto('/');
-</script>
+<svelte:head>
+<meta http-equiv="Refresh" content="0; url='/'" />
+</svelte:head>
+<h1>
+	  Redirecting to <a href="/">search</a>...
+</h1>
