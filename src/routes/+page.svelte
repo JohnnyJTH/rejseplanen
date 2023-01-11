7c8dc8e3feff6e7460ebc92fd7ec4d492695fae0
Johnny JTH <johanohly@gmail.com>
2023-01-11 17:17:55 +0100

    Looks good now

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -23,21 +23,15 @@
 				}
 			});
 		}
-		stations = storageArr;
+		stations = storageArr.slice(0, 11);
 	};
 
-	let searchInput;
 	let inputValue = '';
 
 	$: if (!inputValue) {
 		stations = [];
 		highlightIndex = null;
 	}
-
-	const clearInput = () => {
-		inputValue = '';
-		searchInput.focus();
-	};
 
 	const setInputVal = (stationName) => {
 		inputValue = removeBold(stationName);
@@ -94,37 +88,38 @@
 
 <div class="container mx-auto">
 	<form autocomplete="off" on:submit|preventDefault={submitValue}>
-		<div class="autocomplete">
-			<input id="station-input" type="text" placeholder="Search for a station..." bind:this={searchInput} bind:value={inputValue} on:input={filterStations} />
+		<div class="relative">
+			<div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+				<svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
+			</div>
+			<input type="search" id="station-input" bind:value={inputValue} on:input={filterStations} class="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500" placeholder="Search for a station" required />
+			<button type="submit" class="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">See departures</button>
 		</div>
-
-		<input value="See departures" type="submit" />
-
-		{#if stations.length > 0}
-			<ul id="autocomplete-items-list">
-				{#each stations as name, i}
-					<!-- svelte-ignore a11y-click-events-have-key-events -->
-					<li class="autocomplete-items" class:autocomplete-active={i === highlightIndex} on:click={setInputVal(name)}>
-						{@html name}
-						<button type="button" on:click={setFavorite(removeBold(name))}>
-							{#if favoriteStations.includes(nameToId[removeBold(name)])}
-								<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
-									<path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z" />
-								</svg>
-							{:else}
-								<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
-									<path
-										d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288L8 2.223l1.847 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.565.565 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z" />
-								</svg>
-							{/if}
-						</button>
-					</li>
-				{/each}
-			</ul>
-		{/if}
 	</form>
+	{#if stations.length > 0}
+		<ul id="autocomplete-items-list">
+			{#each stations as name, i}
+				<!-- svelte-ignore a11y-click-events-have-key-events -->
+				<li class="autocomplete-items" class:autocomplete-active={i === highlightIndex} on:click={setInputVal(name)}>
+					{@html name}
+					<button type="button" on:click={setFavorite(removeBold(name))}>
+						{#if favoriteStations.includes(nameToId[removeBold(name)])}
+							<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+								<path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z" />
+							</svg>
+						{:else}
+							<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+								<path
+									d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288L8 2.223l1.847 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.565.565 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z" />
+							</svg>
+						{/if}
+					</button>
+				</li>
+			{/each}
+		</ul>
+	{/if}
 
-	<div class="w-full max-w-md mt-10 md:m-10 bg-white border rounded-lg shadow-md p-4 md:p-8">
+	<div class="w-full max-w-md mt-10 bg-white border rounded-lg shadow-md p-4 md:p-8">
 		<div class="flex items-center justify-between mb-4">
 			<h5 class="text-xl font-bold leading-none text-gray-900">Favorites</h5>
 		</div>
@@ -162,28 +157,6 @@
 </div>
 
 <style>
-	div.autocomplete {
-		/*the container must be positioned relative:*/
-		position: relative;
-		display: inline-block;
-		width: 300px;
-	}
-	input {
-		border: 1px solid transparent;
-		background-color: #f1f1f1;
-		padding: 10px;
-		font-size: 16px;
-		margin: 0;
-	}
-	input[type='text'] {
-		background-color: #f1f1f1;
-		width: 100%;
-	}
-	input[type='submit'] {
-		background-color: DodgerBlue;
-		color: #fff;
-	}
-
 	#autocomplete-items-list {
 		position: relative;
 		margin: 0;
